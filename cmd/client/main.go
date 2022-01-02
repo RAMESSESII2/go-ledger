e56070ea3b67345110fe5a4bdc39ca6618d197de
Ramesses2 <stmbhardwaj@gmail.com>
2022-01-02 20:24:16 +0530

    cmd/client and cmd/server -- entry Packages created and working fine

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"os"
 
+	"github.com/RAMESSESII2/go-ledger/client"
+)
+
+var (
+	backendURIFlag = flag.String("backend", "http://localhost:9000", "Backend API URI")
+	helpFlag       = flag.Bool("help", false, "Display a helpful message")
+)
+
+func main() {
+	flag.Parse()
+	s := client.NewSwitch(*backendURIFlag)
+
+	if *helpFlag || len(os.Args) == 1 {
+		s.Help()
+		return
+	}
+
+	err := s.Switch()
+	if err != nil {
+		fmt.Printf("cmd switch error: %v\n", err)
+		os.Exit(2)
+	}
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,4 +1,7 @@
 package main
 
+import "github.com/RAMESSESII2/go-ledger/server"
+
 func main() {
+	server.StartServer()
 }
