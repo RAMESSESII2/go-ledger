c9bd26bd8094f5e154f31574fd42edb438c365b9
Ramesses2 <stmbhardwaj@gmail.com>
2022-01-02 22:43:54 +0530

    cmd/ refactor

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "github.com/RAMESSESII2/go-ledger/server"
+import (
+	"flag"
+
+	"github.com/RAMESSESII2/go-ledger/server"
+)
+
+// const DNS = "root:World@5261@tcp(127.0.0.1:3306)/ledgerDB?charset=utf8mb4&parseTime=True&loc=Local"
+
+var (
+	addressFlag    = flag.String("port", "9000", "HTTP server address")
+	dbUserFlag     = flag.String("username", "root", "mysql username")
+	dbPasswordFlag = flag.String("password", "World@5261", "user's mysql password")
+)
 
 func main() {
-	server.StartServer()
+	flag.Parse()
+	dbPath := (*dbUserFlag) + ":" + (*dbPasswordFlag) + "@tcp(127.0.0.1:3306)/ledgerDB?charset=utf8mb4&parseTime=True&loc=Local"
+	server.StartServer(dbPath, ":"+*addressFlag)
 }
